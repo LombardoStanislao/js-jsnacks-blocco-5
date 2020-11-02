dabdaacb76949ce0ee323c1b70e7b2c24f9cd1d0
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-11-02 16:31:59 +0100

    Inizializzato snack 2, creati cicli ed array

--- js-snack2/main.js
+++ js-snack2/main.js
@@ -3,5 +3,28 @@
 
 $(document).ready(function() {
 
+// Creo l'array di nomi
+
+    var names = ['Luca', 'Paolo', 'Chiara', 'Giulia', 'Elena', 'Angelo', 'Cinzia', 'Nicola', 'Laura', 'Francesca', 'Claudio'];
+
+// Creo l'array da riempire con il range di nomi
+
+    var chosenName = [];
+
+// Chiedo all'utente un numero
+    var firstUserNumber = parseInt(prompt('Inserisci un numero tra 0 e 11'));
+
+    var secondUserNumber = parseInt(prompt('Inserisci un numero tra 0 e 11'));
+
+    for (var i = 0; i < names.length; i++) {
+
+        if ((i >= firstUserNumber) && (i <= secondUserNumber)) {
+
+            console.log(names[i-1]);
+        }
+    }
+
+
+
 
 });
